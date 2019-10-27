0e425c3d0d0c2db68c423ff821d00a5dc266d5d1
Patrick Woolfenden <hockeywoolf19@hotmail.com>
2019-10-26 19:40:41 -0700

    ports updated to match readme file

--- src/main/java/com/swervedrivespecialties/exampleswerve/RobotMap.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/RobotMap.java
@@ -1,19 +1,19 @@
 package com.swervedrivespecialties.exampleswerve;
 
 public class RobotMap {
-    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 4; // PWM
-    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_ENCODER = 0; // Analog
-    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 10; // CAN
+    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 1; // PWM
+    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_ENCODER = 1; // Analog
+    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 1; // CAN
 
-    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 7; // PWM
-    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_ENCODER = 3; // Analog
-    public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 6; // CAN
+    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 2; // PWM
+    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_ENCODER = 2; // Analog
+    public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 2; // CAN
 
-    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 5; // PWM
-    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_ENCODER = 1; // Analog
-    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 9; // CAN
+    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 3; // PWM
+    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_ENCODER = 3; // Analog
+    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 3; // CAN
 
-    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 6; // PWM
-    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_ENCODER = 2; // Analog
-    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 7; // CAN
+    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 4; // PWM
+    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_ENCODER = 4; // Analog
+    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 4; // CAN
 }
