6ba96c9518b66ed0d8e716777b8ea07b4a6c54d1
Patrick Woolfenden <hockeywoolf19@hotmail.com>
2019-10-26 19:42:45 -0700

    Changed angle offsets to match readme

--- src/main/java/com/swervedrivespecialties/exampleswerve/subsystems/DrivetrainSubsystem.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/subsystems/DrivetrainSubsystem.java
@@ -18,10 +18,10 @@ public class DrivetrainSubsystem extends SwerveDrivetrain {
     private static final double TRACKWIDTH = 19.5;
     private static final double WHEELBASE = 23.5;
 
-    private static final double FRONT_LEFT_ANGLE_OFFSET = -Math.toRadians(0);
-    private static final double FRONT_RIGHT_ANGLE_OFFSET = -Math.toRadians(0);
-    private static final double BACK_LEFT_ANGLE_OFFSET = -Math.toRadians(0);
-    private static final double BACK_RIGHT_ANGLE_OFFSET = -Math.toRadians(0);
+    private static final double FRONT_LEFT_ANGLE_OFFSET = -Math.toRadians(0.0);
+    private static final double FRONT_RIGHT_ANGLE_OFFSET = -Math.toRadians(0.0);
+    private static final double BACK_LEFT_ANGLE_OFFSET = -Math.toRadians(0.0);
+    private static final double BACK_RIGHT_ANGLE_OFFSET = -Math.toRadians(0.0);
 
     private static final Object INSTANCE_LOCK = new Object();
     private static DrivetrainSubsystem instance;
