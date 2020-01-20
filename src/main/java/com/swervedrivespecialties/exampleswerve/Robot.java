0041f624586ff140715c1458956e4a365ea0b5f9
CadeReinberger <32687832+CadeReinberger@users.noreply.github.com>
2020-01-20 10:57:31 -0500

    Revert "Some failed RobotMap Stuff"
    This reverts commit f568b121e522c1053d0f66efa236bef7889baf22.

--- src/main/java/com/swervedrivespecialties/exampleswerve/Robot.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/Robot.java
@@ -2,9 +2,6 @@ package com.swervedrivespecialties.exampleswerve;
 
 import com.swervedrivespecialties.exampleswerve.auton.Trajectories;
 import com.swervedrivespecialties.exampleswerve.subsystems.DrivetrainSubsystem;
-
-import org.frcteam2910.common.robot.UpdateManager;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
@@ -12,11 +9,6 @@ public class Robot extends TimedRobot {
     private static OI oi;
 
     private static DrivetrainSubsystem drivetrain;
-
-    private UpdateManager updateManager = new UpdateManager(
-            DrivetrainSubsystem.getInstance()
-    );
-
 
     public static OI getOi() {
         return oi;
@@ -27,7 +19,6 @@ public class Robot extends TimedRobot {
         oi = new OI();
         drivetrain = DrivetrainSubsystem.getInstance();
         Trajectories.generateAllTrajectories();
-        updateManager.startLoop(5.0e-3);
     }
 
     @Override
